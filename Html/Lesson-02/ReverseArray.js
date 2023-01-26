359f33651f893a85b072b68875e4a245e9621f16
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-01-26 23:30:01 +0530

    reverse array working Code

--- Html/Lesson-02/ReverseArray.js
+++ Html/Lesson-02/ReverseArray.js
@@ -3,9 +3,9 @@ var arr = [1, 2, 3, 4, 5, 6];
 var arrayi = [];
 function rev() {
   var temp;
-  for (i = 0; i <= arr.length; i++) {
-    if (arr[i] < arr(i)) {
-      arrayi.push(arr(i));
-    }
+  for (i = arr.length - 1; i >= 0; i--) {
+    arrayi.push(arr[i]);
   }
+  return arrayi;
 }
+console.log(rev());
