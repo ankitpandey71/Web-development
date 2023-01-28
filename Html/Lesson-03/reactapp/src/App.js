734ff18de37961c3ca6eb281b0d359399f8818d8
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-01-28 12:22:10 +0530

    added multi person props

--- Html/Lesson-03/reactapp/src/App.js
+++ Html/Lesson-03/reactapp/src/App.js
@@ -2,18 +2,23 @@ import React from "react";
 
 const Person = (props) => {
   return (
-    <>
-      <h1>Name:</h1>
-      <h1>Last Name:</h1>
-      <h1>Age:</h1>
-    </>
+    <div>
+      <h1>Name: {props.name}</h1>
+      <h1>Last Name: {props.lastName}</h1>
+      <h1>Age: {props.age}</h1>
+    </div>
   );
 };
 
 const App = () => {
   return (
     <div>
-      <h1>hello World</h1>
+      <Person name={"John"} lastName={"Doe"} age={27} />
+      <Person name={"Don"} lastName={"noe"} age={25} />
+      <Person name={"Kon"} lastName={"joe"} age={28} />
+      <Person name={"JAshhji"} lastName={"moe"} age={21} />
     </div>
   );
 };
+
+export default App;
