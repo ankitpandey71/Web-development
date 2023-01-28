bcac9e3508909de9d82a750a65c6f9ee2f2e7d56
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-01-28 12:12:55 +0530

    added person detail in reactjs

--- Html/Lesson-03/reactapp/src/App.js
+++ Html/Lesson-03/reactapp/src/App.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name:</h1>
+      <h1>Last Name:</h1>
+      <h1>Age:</h1>
+    </>
+  );
+};
+
 const App = () => {
   return (
     <div>
