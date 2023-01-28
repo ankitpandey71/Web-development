a11694241b3fcd3224696a5fe1421309b1389360
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-01-28 12:45:14 +0530

    unchange Removed

--- Html/Lesson-03/reactapp/src/index.js
+++ Html/Lesson-03/reactapp/src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import Hooks from "./Components/Hooks";
 
 ReactDOM.render(<App />, document.getElementById("root"));
