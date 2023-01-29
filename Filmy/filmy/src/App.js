2605498bfbbd18cc6e7a889f4e46014537e68cc1
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-01-29 11:00:17 +0530

    added new Filmy app

---
+++ Filmy/filmy/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=3e3e4bd7";
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+  return (
+    <div>
+      <h1>Filmy</h1>
+      <div>
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src="" onClick={() => {}} />
+      </div>
+      <div>{</div>
+    </div>
+  );
+};
+
+export default App;
