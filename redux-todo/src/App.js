70d26a66d00c5b8ee3cda3dc29e4463852863dde
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-02-06 15:38:51 +0530

    added todo input button

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const App = () => {
-  return <div>TodoList App</div>;
+  return (
+    <div>
+      <h1>TodoList App</h1>
+      <form>
+        <input placeholder="enter todo here" />
+        <button type="submit">+</button>
+      </form>
+      <ul>
+        <li>
+          <span>First Todo</span>
+          <button>Delete</button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
