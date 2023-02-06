d0091fb497ea64626881ba93c207f78aa2d4ac4d
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-02-06 14:08:22 +0530

    redux todo

---
+++ redux-todo/src/Store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const reducer = combineReducers({
+  // this containes all reducer
+});
+
+const initialState = {};
+
+const applyMiddleware = [thunk];
+
+const store = createStore(reducer, initialState);
